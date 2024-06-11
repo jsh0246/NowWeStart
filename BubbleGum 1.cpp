bd64ff972800433c73df4d16fb5b9d4736b3f632
jsh0246 <jsh0246@naver.com>
2024-06-11 16:34:13 +0900

    초기 커밋.

---
+++ BubbleGum 1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hanni Minji Hyerin " << endl;
+	return 0;
+}
---
+++ HowSweet 1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "How Sweet" << endl;
+	return 0;
+}
