88ba06fd31fdc868f7de4906f840a5362fa90092
jsh0246 <jsh0246@naver.com>
2024-06-13 09:52:05 +0900

    dd

--- Qsort.cpp
+++ Qsort.cpp
@@ -7,10 +7,23 @@ priority_queue<int> q;
 
 int main()
 {
+	int a = 1;
+	int b = 10;
 
-	q.push(1);
-	q.push(2);
-	q.push(3);
+
+	q.push(a);
+	q.push(b);
+
+	a = 100;
+	
+
+	while(!q.empty()) {
+		cout << q.top() << endl;
+		q.pop();
+	}
+
+	
+
 
 
 
